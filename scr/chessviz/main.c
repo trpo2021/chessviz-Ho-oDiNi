dc598ceeb9c6affef535f1e6a853736b8818259b
Ho-oDiNi <78462812+Ho-oDiNi@users.noreply.github.com>
2021-05-03 15:59:04 +0500

    Update main.c

--- scr/chessviz/main.c
+++ scr/chessviz/main.c
@@ -1,26 +1,40 @@
-#include<stdio.h>
+#include<iostream>
+#include <stdlib.h>
+#include "func.h"
 
-int main()
+using namespace std; 
+ 
+char buff; // буфер хранения фигуры
+bool flag_buff = false; // флаг фигуры
+char table[9][9]; // массив представления фигур
+bool table_b[9][9]; // массив нахождения фигур
+bool table_h[9][9]; // массив хода фигур
+int sum_hod = 0, y_error, x_error; // полуходы
+ 
+int main() 
 {
-	char *board[8][8] = 
-	{
-		{"r", "n", "b", "q", "k", "b", "n", "r"},
-		{"p", "p", "p", "p", "p", "p", "p", "p"},
-		{" ", " ", " ", " ", " ", " ", " ", " "},
-		{" ", " ", " ", " ", " ", " ", " ", " "},
-		{" ", " ", " ", " ", " ", " ", " ", " "},
-		{" ", " ", " ", " ", " ", " ", " ", " "},
-		{"P", "P", "P", "P", "P", "P", "P", "P"}, 
-		{"R", "N", "B", "Q", "K", "B", "N", "R"}
-	};
-	int i, j;
-    for (i = 0; i < 8; i++)
-	{
-		printf("\n");
-        for (j = 0; j < 8; j++)
-		{
-        	printf("%c ", *board[i][j]); 
-    	}
-    }
+    setlocale(0, "Russian");
+    table_default();    
+    int y = 0, error = 1, y_error2 = 0;
+    char x = 0, x_error2 = 0;
+    do
+    {
+    	cin >> x >> y;
+        error = table_hod(x, y);
+        if(error==0)
+        {
+        	cin >> x_error2 >> y_error2;
+        	PrintTable();  
+			cout  << "\n\nВы не взяли фигуру" << endl;
+			cout  << "Введите ход заного" << endl;   
+		}	
+        if(error==-1)
+        {
+        	PrintTable(); 
+        	error = table_hod(x_error, y_error); 
+			cout  << "\n\nФигура так не ходит" << endl;
+			cout  << "Введите ход заного" << endl;
+		}
+	} while (x,y);         
     return 0;
 }
